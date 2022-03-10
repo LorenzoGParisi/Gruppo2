750166e4ba0024f6abadf231c75f4add0a6534a2
Leonardo Cionchi <leonardo.cionchi@edu.itspiemonte.it>
2022-03-10 10:55:19 +0100

    fatto

--- GitHubJavaProgetto/src/Hello/HelloWorld.java
+++ GitHubJavaProgetto/src/Hello/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Ciao sono LorenzoGParisi");
 		System.out.println("Ciao sono Marcello Borio");
+		System.out.println("Ciao sono Leonardo Cionchi");
 	}
 }
